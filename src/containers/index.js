ed7ce3ed5d1ac0f873fbc180ce96b8fe6ffb8118
Steven Barash <steven.barash@descope.com>
2025-09-09 14:29:37 -0400

    fixes

--- src/containers/index.js
+++ src/containers/index.js
@@ -12,6 +12,7 @@ import Profile from "../dashboard/profile/profile";
 import Apps from "../dashboard/apps/apps";
 import AccessKeys from "../dashboard/access-keys/accesskeys";
 import TransferFunds from "../dashboard/transfer/TransferFunds";
+import Onboarding from "../dashboard/onboarding/Onboarding";
 
 
 const Container = () => {
@@ -28,6 +29,7 @@ const Container = () => {
         <Route path="user/apps" element={<Apps />} />
         <Route path="user/keys" element={<AccessKeys/>} />
         <Route path="user/transfer-funds" element={<TransferFunds />} />
+        <Route path="user/onboarding" element={<Onboarding />} />
 
       </Route>
 
